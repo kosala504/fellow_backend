961ea204f70a10cb2a61475934ce0228a53a2709
kosala504 <kosala504@gmail.com>
2025-01-18 09:49:30 +0530

    Fixed depoly errors

--- config/server.ts
+++ config/server.ts
@@ -2,6 +2,6 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['yourRandomKey1', 'yourRandomKey2']), /*<Before version>keys: env.array('APP_KEYS'),*/
   },
 });
